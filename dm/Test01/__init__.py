b073d7b880cf7f713dd292d8d2d6135d860d646e
xctg004 <xctg004>
2020-08-25 05:56:25 +0000

    Model Designer: (auto) commit generated configuration

--- dm/Test01/__init__.py
+++ dm/Test01/__init__.py
@@ -3,6 +3,7 @@ import digital_machine
 import digital_machine.templates as tmpl
 from digital_machine import dmCompile
 
+from .computer_root import computer_root
 from .Furnace_Blast import Furnace_Blast
 from .workshop import workshop
 
@@ -16,6 +17,7 @@ py_model_collection = tmpl.PyModelCollection()
 
 
 
+computer_root().generate_model(digital_machine.digital_twin_model("Test01", "computer_root"), py_model_collection)
 Furnace_Blast().generate_model(digital_machine.digital_twin_model("Test01", "Furnace_Blast"), py_model_collection)
 workshop().generate_model(digital_machine.digital_twin_model("Test01", "workshop"), py_model_collection)
 
